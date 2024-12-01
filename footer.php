bdc4674c1afd1f2bada0022bd53ce32dfd0f09c9
Lewis Ushindi <frenziecodes@gmail.com>
2024-12-01 09:09:02 +0300

    improvements

--- footer.php
+++ footer.php
@@ -6,16 +6,16 @@
 
     <footer id="colophon" class="site-footer">
         <div class="site-info">
-            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'astra' ) ); ?>">
+            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nordiskaspel' ) ); ?>">
                 <?php
-                /* translators: %s: CMS name, i.e. WordPress. */
-                printf( esc_html__( 'Proudly powered by %s', 'astra' ), 'WordPress' );
+                /* translators: %s: CMS name, i.e. WordPress....se. */
+                printf( esc_html__( 'Proudly powered by %s', 'nordiskaspel' ), 'WordPress' );
                 ?>
             </a>
             <span class="sep"> | </span>
                 <?php
                 /* translators: 1: Theme name, 2: Theme author. */
-                printf( esc_html__( 'Theme: %1$s by %2$s.', 'astra' ), 'Astra', '<a href="http://wpastra.com/">Brainstorm Force</a>' );
+                printf( esc_html__( 'Theme: %1$s by %2$s.', 'nordiskaspel' ), 'nordiskaspel', '<a href="http://wpNordiskaspel.com/">Brainstorm Force</a>' );
                 ?>
         </div><!-- .site-info -->
     </footer><!-- #colophon -->
